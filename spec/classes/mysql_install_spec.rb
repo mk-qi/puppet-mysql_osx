b813a4f2732f51235c5cf2eb491ceff2317f05ee
Björn Albers <bjoernalbers@googlemail.com>
2012-08-27 10:44:33 +0200

    Fix class name for mysql::install spec

--- spec/classes/mysql_install_spec.rb
+++ spec/classes/mysql_install_spec.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
 
-describe 'mysql' do
-  describe 'on Mac OS X' do
-    it { should contain_package('mysql').with(
-      :ensure   => 'present',
-      :provider => 'brew',
-      :require  => 'User[_mysql]'
-    )}
+describe 'mysql::install' do
+  it { should contain_package('mysql').with(
+    :ensure   => 'present',
+    :provider => 'brew',
+    :require  => 'User[_mysql]'
+  )}
 
-    it { should contain_group('_mysql').with(
-      :ensure => 'present'
-    )}
+  it { should contain_group('_mysql').with(
+    :ensure => 'present'
+  )}
 
-    it { should contain_user('_mysql').with(
-      :ensure  => 'present',
-      :comment => 'MySQL User',
-      :home    => '/var/empty',
-      :shell   => '/usr/bin/false',
-      :gid     => '_mysql'
-    )}
+  it { should contain_user('_mysql').with(
+    :ensure  => 'present',
+    :comment => 'MySQL User',
+    :home    => '/var/empty',
+    :shell   => '/usr/bin/false',
+    :gid     => '_mysql'
+  )}
 
-    it { should contain_exec('mysql_install_db').with(
-      :command   => '/usr/local/bin/mysql_install_db --verbose --user=_mysql --basedir="$(/usr/local/bin/brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp',
-      :creates   => '/usr/local/var/mysql',
-      :logoutput => 'on_failure',
-      :require   => 'Package[mysql]'
-    )}
-  end
+  it { should contain_exec('mysql_install_db').with(
+    :command   => '/usr/local/bin/mysql_install_db --verbose --user=_mysql --basedir="$(/usr/local/bin/brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp',
+    :creates   => '/usr/local/var/mysql',
+    :logoutput => 'on_failure',
+    :require   => 'Package[mysql]'
+  )}
 end
